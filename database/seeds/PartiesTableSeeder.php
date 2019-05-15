cf6cdea4a98308b74e3fea8d95cbe5a6b32314eb
Dortund <noneof@yourbusiness.com>
2019-05-15 14:51:36 +0200

    rearranged some party colours to be more clear

--- database/seeds/PartiesTableSeeder.php
+++ database/seeds/PartiesTableSeeder.php
@@ -12,21 +12,21 @@ class PartiesTableSeeder extends Seeder
      */
     public function run()
     {
-        self::insert('Octopus', '#009900');
+        self::insert('Octopus', '#009999');
         self::insert('Kurk', '#990000');
-        self::insert('Quast', '#000099');
+        self::insert('Quast', '#FFFF33');
         self::insert('Schranz', '#999900');
-        self::insert('Spetter', '#009999');
+        self::insert('Spetter', '#000099');
         self::insert('Asene', '#990099');
-        self::insert('Apollo', '#33FF99');
+        self::insert('Apollo', '#00FFFF');
         self::insert('Chaos', '#FF8000');
         self::insert('Krat', '#808080');
         self::insert('McClan', '#00FF00');
         self::insert('Kielzog', '#000000');
-        self::insert('Nobel', '#0000FF');
-        self::insert('Scorpios', '#FF0000');
+        self::insert('Nobel', '#FF0000');
+        self::insert('Scorpios', '#0000FF');
         self::insert('Fabula', '#FF00FF');
-        self::insert('Bestuur 122', '#00FFFF');
+        self::insert('Bestuur 122', '#009900');
         self::insert('OC19', '#FFFF00');
     }
     
